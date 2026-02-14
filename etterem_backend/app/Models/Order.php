6ab64c80185c326d2fb3e7b85511f4a2f8ef7e9d
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2026-02-14 15:15:15 +0100

    Order model es migracio

---
+++ etterem_backend/app/Models/Order.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'table_id',
+        'waiter_id',
+        'total_price',
+        'status',
+    ];
+
+    protected $casts = [
+        'total_price' => 'integer',
+    ];
+
+    public function table() {
+        return $this->belongsTo(Table::class);
+    }
+
+    public function waiter() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems() {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function payments() {
+        return $this->hasMany(Payment::class);
+    }
+}
---
+++ etterem_backend/database/migrations/2026_02_14_125058_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('table_id')->constrained('tables')->onDelete('cascade');
+            $table->foreignId('waiter_id')->constrained('users')->onDelete('cascade');
+            $table->unsignedInteger('total_price');
+            $table->enum('status', ['waiting_for_service', 'eating', 'ready_to_pay', 'done'])->default('waiting_for_service');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
