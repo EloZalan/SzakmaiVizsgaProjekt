f466479faa53f91cbe89aee9e3a382fb15ff864b
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2026-03-10 16:23:19 +0100

    vegpontok modositasa

--- etterem_backend/routes/api.php
+++ etterem_backend/routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/reservations', [ReservationController::class, 'store']);
 
+Route::get('/menu-categories', [\App\Http\Controllers\MenuCategoryController::class, 'index']);
+Route::get('/menu-categories/{menu_category}', [\App\Http\Controllers\MenuCategoryController::class, 'show']);
+
+Route::get('/menu-items', [MenuItemController::class, 'index']);
+Route::get('/menu-items/{menu_item}', [MenuItemController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'me']);
     Route::put('/user', [AuthController::class, 'update']);
@@ -27,13 +33,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('tables/{table}', [TableController::class, 'show']);
     Route::post('/tables/{table}/orders', [OrderController::class, 'openOrder']);
 
-    // public menu category endpoints
-    Route::get('/menu-categories', [\App\Http\Controllers\MenuCategoryController::class, 'index']);
-    Route::get('/menu-categories/{menu_category}', [\App\Http\Controllers\MenuCategoryController::class, 'show']);
 
-    // public menu item endpoints
-    Route::get('/menu-items', [MenuItemController::class, 'index']);
-    Route::get('/menu-items/{menu_item}', [MenuItemController::class, 'show']);
     Route::post('/orders/{order}/items', [OrderController::class, 'addItem']);
     Route::post('/orders/{order}/simulate-ready', [OrderController::class, 'simulateReadyToPay']);
     Route::post('/orders/{order}/pay', [PaymentController::class, 'pay']);
@@ -47,12 +47,10 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('/admin/tables/{table}', [TableController::class, 'update']);
         Route::delete('/admin/tables/{table}', [TableController::class, 'destroy']);
 
-        // menu categories management (admin only)
         Route::post('/admin/menu-categories', [\App\Http\Controllers\MenuCategoryController::class, 'store']);
         Route::put('/admin/menu-categories/{menu_category}', [\App\Http\Controllers\MenuCategoryController::class, 'update']);
         Route::delete('/admin/menu-categories/{menu_category}', [\App\Http\Controllers\MenuCategoryController::class, 'destroy']);
 
-        // menu items management (admin only)
         Route::post('/admin/menu-items', [MenuItemController::class, 'store']);
         Route::put('/admin/menu-items/{menu_item}', [MenuItemController::class, 'update']);
         Route::delete('/admin/menu-items/{menu_item}', [MenuItemController::class, 'destroy']);
