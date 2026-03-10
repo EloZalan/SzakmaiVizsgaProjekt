dc09f6f1659831c1e9f4c1f854bcb15fae58a6e0
fazekasbotond3 <fazekas.botond@students.jedlik.eu>
2026-03-10 12:22:34 +0100

    moved components

--- etterem_frontend/grillhouse/src/app/app.routes.ts
+++ etterem_frontend/grillhouse/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page';
-import { WaiterPageComponent } from './pages/waiter-page/waiter-page';
+import { LoginPageComponent } from './components/login-page/login-page';
+import { WaiterPageComponent } from './components/waiter-page/waiter-page';
 import { roleGuard } from './guards/role-guard';
-import { AdminPageComponent } from './pages/admin-page/admin-page';
+import { AdminPageComponent } from './components/admin-page/admin-page';
 import { ReserevePage } from './components/resereve-page/resereve-page';
 
 export const routes: Routes = [
