02c164703d3504f1bfccb6b833ba81c320d1e70c
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2026-04-03 15:50:10 +0200

    swaggerhez

--- etterem_backend/routes/web.php
+++ etterem_backend/routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs', function () {
+    return view('swagger-ui');
+});
+
+Route::get('/docs/openapi.yaml', function () {
+    $specPath = base_path('swagger.yaml');
+
+    if (!file_exists($specPath)) {
+        abort(404, 'Swagger spec file not found.');
+    }
+
+    return response()->file($specPath, [
+        'Content-Type' => 'application/yaml; charset=UTF-8',
+        'Cache-Control' => 'no-store, no-cache, must-revalidate, max-age=0',
+    ]);
+});
